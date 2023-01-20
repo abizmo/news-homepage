41696dbf4abce7b281dda965b5f706278065e68f
Abián Izquierdo <abizmo@gmail.com>
2023-01-20 15:40:09 +0000

    Initial structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -7,8 +8,146 @@ const Home: NextPage = () => {
       <Head>
         <title>News Homepage</title>
       </Head>
+      <header>
+        <div>
+          <a href="#">
+            <svg className="icon">
+              <use xlinkHref="/images/logo.svg#logo"></use>
+            </svg>
+          </a>
+          <button>Menu</button>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">New</a>
+              </li>
+              <li>
+                <a href="#">Popular</a>
+              </li>
+              <li>
+                <a href="#">Trending</a>
+              </li>
+              <li>
+                <a href="#">Categories</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
       <main>
-        <h1>News Homepage</h1>
+        <div>
+          <section id="hero">
+            <picture>
+              <source
+                srcSet="/images/image-web-3-desktop.jpg"
+                media="(min-width: 70em)"
+              />
+              <img src="/images/image-web-3-mobile.jpg" alt="Hero Image" />
+            </picture>
+            <h1>The Bright Future of Web 3.0?</h1>
+            <div>
+              <p>
+                We dive into the next evolution of the web that claims to put
+                the power of the platforms back into the hands of the people.
+                But is it really fulfilling its promise?
+              </p>
+              <button>Read more</button>
+            </div>
+          </section>
+          <section>
+            <h2>New</h2>
+            <ul>
+              <li>
+                <div>
+                  <a href="#">
+                    <h3>Hydrogen VS Electric Cars</h3>
+                  </a>
+                  <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="#">
+                    <h3>The Downsides of AI Artistry</h3>
+                  </a>
+                  <p>
+                    What are the possible adverse effects of on-demand AI image
+                    generation?
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="#">
+                    <h3>Is VC Funding Drying Up?</h3>
+                  </a>
+                  <p>
+                    Private funding by VC firms is down 50% YOY. We take a look
+                    at what that means.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </section>
+        </div>
+        <section>
+          <ul>
+            <li>
+              <div>
+                <Image
+                  src="/images/image-retro-pcs.jpg"
+                  alt="small computer"
+                  width={100}
+                  height={128}
+                />
+                <div>
+                  <h2>01</h2>
+                  <a href="#">
+                    <h3>Reviving Retro PCs</h3>
+                  </a>
+                  <p>What happens when old PCs are given modern upgrades?</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <Image
+                  src="/images/image-top-laptops.jpg"
+                  alt="laptop keys"
+                  width={100}
+                  height={128}
+                />
+                <div>
+                  <h2>02</h2>
+                  <a href="#">
+                    <h3>Top 10 Laptops of 2022</h3>
+                  </a>
+                  <p>Our best picks for various needs and budgets.</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <Image
+                  src="/images/image-gaming-growth.jpg"
+                  alt="dualshock"
+                  width={100}
+                  height={128}
+                />
+                <div>
+                  <h2>03</h2>
+                  <a href="#">
+                    <h3>The Growth of Gaming</h3>
+                  </a>
+                  <p>How the pandemic has sparked fresh opportunities.</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </section>
       </main>
       <footer className="attribution">
         Challenge by{" "}
@@ -19,7 +158,7 @@ const Home: NextPage = () => {
         >
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by <a href="https://www.abizmo.dev">Abián Izquierdo</a>.
       </footer>
     </>
   );
