9ea81b8be4ed9ee85db23d35d4d2fd3956373d19
Abián Izquierdo <abizmo@gmail.com>
2023-01-25 21:08:00 +0000

    Style the main section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,26 @@ import Image from "next/image";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+const news = [
+  {
+    url: "#",
+    title: "Hydrogen VS Electric Cars",
+    description: "Will hydrogen-fueled cars ever catch up to EVs?",
+  },
+  {
+    url: "#",
+    title: "The Downsides of AI Artistry",
+    description:
+      "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    url: "#",
+    title: "Is VC Funding Drying Up?",
+    description:
+      "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+  },
+];
+
 const Home: NextPage = () => {
   return (
     <>
@@ -12,59 +32,51 @@ const Home: NextPage = () => {
         <title>News Homepage</title>
       </Head>
       <Header />
-      <main>
-        <div>
-          <section id="hero">
-            <picture>
+      <main className="container mx-auto px-4 lg:px-0">
+        <div className="lg:mb-18 mb-16 lg:flex lg:gap-7">
+          <section id="hero" className="mb-16 lg:mb-0">
+            <picture className="mb-6 block lg:mb-9">
               <source
                 srcSet="/images/image-web-3-desktop.jpg"
                 media="(min-width: 70em)"
               />
               <img src="/images/image-web-3-mobile.jpg" alt="Hero Image" />
             </picture>
-            <h1>The Bright Future of Web 3.0?</h1>
-            <div>
-              <p>
-                We dive into the next evolution of the web that claims to put
-                the power of the platforms back into the hands of the people.
-                But is it really fulfilling its promise?
-              </p>
-              <button>Read more</button>
+
+            <div className="lg:flex lg:gap-7">
+              <div className="lg:flex-1">
+                <h1 className="mb-4 max-w-xs text-4xl font-black text-very-dark-blue lg:mb-0 lg:text-5xl">
+                  The Bright Future of Web 3.0?
+                </h1>
+              </div>
+              <div className="lg:flex-1">
+                <p className="mb-6 max-w-xs lg:mb-9">
+                  We dive into the next evolution of the web that claims to put
+                  the power of the platforms back into the hands of the people.
+                  But is it really fulfilling its promise?
+                </p>
+                <button className="bg-soft-red px-8 py-4 text-sm font-bold uppercase text-off-white">
+                  Read more
+                </button>
+              </div>
             </div>
           </section>
-          <section>
-            <h2>New</h2>
+          <section className="bg-very-dark-blue px-5 py-6 lg:px-6 lg:py-8">
+            <h2 className="mb-2 text-3xl font-bold text-soft-orange lg:text-4xl">
+              New
+            </h2>
             <ul>
-              <li>
-                <div>
-                  <a href="#">
-                    <h3>Hydrogen VS Electric Cars</h3>
-                  </a>
-                  <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <a href="#">
-                    <h3>The Downsides of AI Artistry</h3>
-                  </a>
-                  <p>
-                    What are the possible adverse effects of on-demand AI image
-                    generation?
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <a href="#">
-                    <h3>Is VC Funding Drying Up?</h3>
-                  </a>
-                  <p>
-                    Private funding by VC firms is down 50% YOY. We take a look
-                    at what that means.
-                  </p>
-                </div>
-              </li>
+              {news.map(({ url, title, description }) => (
+                <li
+                  key={title}
+                  className="border-dark-grayish-blue py-7 last:pb-0 [&:not(:last-child)]:border-b"
+                >
+                  <h3 className="mb-2 text-2xl font-bold text-off-white lg:mb-3">
+                    <a href={url}>{title}</a>
+                  </h3>
+                  <p>{description}</p>
+                </li>
+              ))}
             </ul>
           </section>
         </div>
