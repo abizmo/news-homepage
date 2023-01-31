c66eb616ebf350809f9d31b3e47a2d8b8d501552
abizmo <abizmo@gmail.com>
2023-01-31 11:39:18 +0000

    Fix: avoid magical numbers

--- src/components/NewsWithImageList.tsx
+++ src/components/NewsWithImageList.tsx
@@ -4,11 +4,19 @@ interface NewsWithImageListProps {
   news: NewsWithImage[];
 }
 
+const IMAGE_HEIGHT = 128;
+const IMAGE_WIDTH = 100;
+
 const NewsWithImageList: React.FC<NewsWithImageListProps> = ({ news }) => (
   <ul className="grid gap-8 lg:grid-cols-3 lg:gap-7">
     {news.map(({ image, url, id, title, description }) => (
       <li key={title} className="flex gap-6">
-        <Image src={image.src} alt={image.alt} width={100} height={128} />
+        <Image
+          src={image.src}
+          alt={image.alt}
+          width={IMAGE_WIDTH}
+          height={IMAGE_HEIGHT}
+        />
         <div className="grid gap-3">
           <h2 className="mb-1 text-3xl font-bold tracking-wide text-grayish-blue">
             {id}
